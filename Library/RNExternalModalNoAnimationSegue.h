4c960e35e1f5d6ded140eb82f4f50a8ea6de5060
Leonir Alves <leonir.alves@qranio.com>
2017-01-24 17:15:02 -0200

    Subclasse RNExternalModalNoAnimationSegue para gerar transições modal sem animação

---
+++ Library/RNExternalModalNoAnimationSegue.h
@@ -0,0 +1,14 @@
+//
+//  RNExternalModalNoAnimationSegue.h
+//  Pods
+//
+//  Created by Leonir Alves on 1/24/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "RNExternModalSegue.h"
+
+@interface RNExternalModalNoAnimationSegue : RNExternModalSegue
+
+@end
